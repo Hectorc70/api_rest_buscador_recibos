c025871ab16a805f4bd7693d43307d619b856e37
Hector Garcia Nieves <hecgarcia.n@gmail.com>
2021-03-30 23:19:45 -0600

    ignore

--- buscador/views.py
+++ buscador/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
 
 from .models import Recibo, Empleado
 from .serializers import EmpleadoSerializer, ReciboSerializer
@@ -23,6 +25,18 @@ class ReciboRecordView(APIView):
 
         return Response(serializer.data)
 
+    def post(self, request, format=None):
+        serializer = ReciboSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            serializer.save()
+            return Response(serializer.data, 
+                            status= status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, 
+                            status= status.HTTP_400_BAD_REQUEST)
+
 
 class EmpleadoRecordView(APIView):
     """
@@ -40,5 +54,14 @@ class EmpleadoRecordView(APIView):
         print(serializer)
         return Response(serializer.data)
 
+    def post(self, request, format=None):
+        serializer = EmpleadoSerializer(data=request.data)
 
+        if serializer.is_valid():
 
+            serializer.save()
+            return Response(serializer.data, 
+                            status= status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, 
+                            status= status.HTTP_400_BAD_REQUEST)
