ca11147a0223407736d56f1f6e64fa723a288bea
Hector Garcia Nieves <hecgarciann@gmail.com>
2021-06-08 12:30:33 -0500

    Se creo View para que retorne el archivo del recibo de nomina

--- buscador/urls.py
+++ buscador/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
-from .views import ReciboRecordView, EmpleadoRecordView, EmpleadoRecordNameView
+from .views import ReciboRecordView, ReciboFileRecordView ,EmpleadoRecordView, EmpleadoRecordNameView
 
 app_name = 'buscador'
 urlpatterns = [
     path('recibo/<control>/<periodo>', ReciboRecordView.as_view(), name='recibo-periodo'),
+    path('recibo-file/<id_recibo>', ReciboFileRecordView.as_view(), name='recibo-file'),
     path('recibo/', ReciboRecordView.as_view(), name='recibo'),
 
     path('empleado/<control>', EmpleadoRecordView.as_view(), name='empleado-control'),
